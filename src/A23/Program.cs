48b58576de2878b28e102686f319f93342578807
matthew <matthewtoday@gmail.com>
2024-12-24 06:13:21 +1000

    AoC 23-2

--- src/A23/Program.cs
+++ src/A23/Program.cs
@@ -5,6 +5,4 @@ using A23;
 var baseDir = Environment.GetEnvironmentVariable("AOC_BaseDir");
 
 var data = File.ReadAllLines(Path.Combine(baseDir!, "A23.data.txt"));
-
-var count = Solution.Solve('t', data);
-Console.WriteLine(count);
+Solution.Solve(data);
--- src/A23/Solution.cs
+++ src/A23/Solution.cs
@@ -2,43 +2,74 @@ namespace A23;
 
 public static class Solution
 {
-    public static int Solve(char t, string[] data)
+    public static bool IsFullyConnected(Dictionary<string, List<string>> graph, List<string> nd)
     {
-        var dict = new Dictionary<string, HashSet<string>>();
-        foreach (var kv in data
-                 .Select(l => l.Split('-')))
+        for (var i = 0; i < nd.Count - 1; i++)
         {
-            if(!dict.TryGetValue(kv[0], out var p0))
+            for (var j = i + 1; j < nd.Count; j++)
             {
-                p0 = [];
-                dict[kv[0]] = p0;
+                var n1 = nd[i];
+                var n2 = nd[j];
+                if (!graph[n1].Contains(n2)) return false;
             }
-            if(!dict.TryGetValue(kv[1], out var p1))
-            {
-                p1 = [];
-                dict[kv[1]] = p1;
-            }
-
-            p0.Add(kv[1]);
-            p1.Add(kv[0]);
         }
 
-        return Solve(t, dict);
+        return true;
     }
-    
-    public static int Solve(char t, Dictionary<string, HashSet<string>> pairs)
+
+    public static void Solve(string[] lines)
     {
-        var hashset = new HashSet<string>();
-        foreach (var n0 in pairs.Where(k =>
-                     k.Key.StartsWith(t)).SelectMany(k => k.Value.Select(v => (k.Key, v))))
+        List<string> tComp = [];
+        Dictionary<string, List<string>> graph = new();
+        foreach (var line in lines.Select(l => l.Split("-")))
         {
-            foreach (var n1 in pairs[n0.v].Where(n => n != n0.Key).Where(n => pairs[n].Contains(n0.Key)))
+            foreach (var l in line)
             {
-                List<string> s = [n0.Key, n0.v, n1];
-                hashset.Add(string.Join("", s.Order()));
+                if (graph.ContainsKey(l)) continue;
+                graph[l] = [];
+                if (l[0] == 't') tComp.Add(l);
             }
+
+            graph[line[0]].Add(line[1]);
+            graph[line[1]].Add(line[0]);
         }
 
-        return hashset.Count;
+        var count = tComp.SelectMany(
+                tn =>
+                    graph[tn].SelectMany(n1 =>
+                        from n2 in graph[tn]
+                        where n1 != n2
+                        where graph[n1].Contains(n2)
+                        select (List<string>) [tn, n1, n2]))
+            .Select(g => string.Join("", g.Order()))
+            .Distinct()
+            .Count();
+
+        var maxPwd = "";
+        var nodes = graph.Keys.OrderBy(k => graph[k].Count).ToList();
+        foreach (var n1 in nodes)
+        {
+            foreach (var nb in graph[n1])
+            {
+                List<string> group =
+                [
+                    n1,
+                    nb
+                ];
+                group.AddRange(graph[n1].Where(nb2 => graph[nb].Contains(nb2)));
+
+                if (!IsFullyConnected(graph, group)) continue;
+                var pwd = string.Join(",", group.Order());
+                if (pwd.Length > maxPwd.Length)
+                {
+                    maxPwd = pwd;
+                }
+                break;
+            }
+
+        }
+
+        Console.WriteLine($"{count}");
+        Console.WriteLine($"{maxPwd}");
     }
 }
