783245122d0720bbfca93976e6ee48ac73740028
matthew <matthewtoday@gmail.com>
2024-12-25 07:19:11 +1000

    AoC 22-2

--- src/A22/Program.cs
+++ src/A22/Program.cs
@@ -13,47 +13,49 @@
    Calculate the result of multiplying the secret number by 2048.
    Then, mix this result into the secret number.
    Finally, prune the secret number.
-
-    (((SN xor (SN * 2^6)) & (2^24 - 1) xor ((SN xor (SN * 2^6)) & (2^24 - 1) * 2^-5)) & (2^24 - 1) xor (((SN xor (SN * 2^6)) & (2^24 - 1) xor ((SN xor (SN * 2^6)) & (2^24 - 1) * 2^-5)) & (2^24 - 1) * 2^11)) & (2^24 - 1)
  */
-
-using System.Numerics;
 
 var baseDir = Environment.GetEnvironmentVariable("AOC_BaseDir");
 var seeds = File.ReadAllLines(Path.Combine(baseDir!, "A22.data.txt"));
 
-Console.WriteLine(seeds.Select(BigInteger.Parse).Sum(s => Solution.Calculate(s, 2000)));
+Console.WriteLine(seeds.Select(long.Parse).Sum(s => Solution.Calculate(s, 2000)));
+var bananas = Solution.BaNaNaS.Max(b => b.Value);
+Console.WriteLine(bananas);
 
 public static class Solution
 {
-    public static BigInteger Mask = (1L << 24) - 1;
+    public static long Mask = (1L << 24) - 1;
+    public record Seq(long A, long B, long C, long D);
+    public static Dictionary<Seq, long> BaNaNaS = new();
 
-    public static BigInteger Calculate(BigInteger n, int m)
+    public static long Calculate(long n, int m)
     {
+        var sequenced = new HashSet<Seq>();
+        List<long> sx = [0, 0, 0, 0];
+        
         for (var i = 0; i < m; ++i)
         {
-            n = Calculate(n);
+            var n1 = Calculate(n);
+            sx[i % 4] = (n1 % 10) - (n % 10);
+            n = n1;
+            
+            if (i < 3) continue;
+            
+            var sequence = new Seq(sx[(i-3)%4], sx[(i-2)%4], sx[(i-1)%4], sx[i%4]);
+            if (!sequenced.Add(sequence)) continue;
+            
+            BaNaNaS.TryGetValue(sequence, out var b);
+            BaNaNaS[sequence] = b + (n1 % 10);
         }
 
         return n;
     }
     
-    public static BigInteger Calculate(BigInteger n)
+    public static long Calculate(long n)
     {
         n = (n ^ (n << 6)) & Mask;
         n = (n ^ (n >> 5)) & Mask;
         n = (n ^ (n << 11)) & Mask;
         return n;
-    }
-    
-    public static BigInteger Sum(this IEnumerable<BigInteger> numbers, Func<BigInteger, BigInteger> fn)
-    {
-        var s = new BigInteger(0);
-        foreach (var n in numbers.Select(fn))
-        {
-            s += n;
-        }
-
-        return s;
     }
 }
